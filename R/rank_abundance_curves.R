9ed1a6b5e22d73a86c007fc155a810c181ffaee1
Poweshiek Skipperling <49678532+tamarahartke@users.noreply.github.com>
2020-10-29 16:38:56 +0100

    version 0.20. -fixes errorreading spec.ranks output in ra.plot -ra.plot infers grouping variable from list rather than requiring gv - adds default colour palette for ra.plot

--- R/rank_abundance_curves.R
+++ R/rank_abundance_curves.R
@@ -1,22 +1,22 @@
 # Functions to calculate rank abundances and plot rank abundance curves
 
 # rank the abundances
-spec.ranks <- function(df,gv){
-  ra.all <- by(df, gv,
-     function(x){
-       t_data <- t(x)
-       sum_data <- rowSums(t_data)
-       ranks <- rank(-sum_data, ties.method = "random")
-     as.data.frame(cbind(sum_data, ranks))
-     } )
-  }
+spec.ranks <- function(df,gv){ # takes community matrix and grouping variable
+  ra.all <- by(df, gv, function(x) { #group data by grouping variable, then
+    t_data <- t(x) # transpose data (sites now columns, species rows)
+    sum_data <- rowSums(t_data) # total abundance for each species
+    ranks <- rank(-sum_data, ties.method = "random") # most abund = rank 1
+    as.data.frame(cbind(sum_data, ranks)) # data frame for each level of gv
+  })
+  ra.all.done <- unclass(ra.all)  # remove the "S3: by" attribute, which made the list unreadable to dplyr::bind_rows in R v4.X
+}
 
 
 # plotting: x = species rank, y = abundance
-ra.plot <- function(ra.df, gv, mycolours){
+ra.plot <- function(ra.df, mycolours = viridis::viridis(length(ra.df))){
   library(ggplot2) # requires ggplot2 for plotting
   plot.df <- dplyr::bind_rows(ra.df, .id="Groups")
-  plot.df$Groups <- factor(plot.df$Groups, labels = c(levels(gv)))
+  plot.df$Groups <- factor(plot.df$Groups, labels = c(names(ra.df[])))
   ggplot(plot.df, aes(x= ranks, y = sum_data, colour = Groups)) +
     geom_line(size = 1) +
     scale_color_manual(values = mycolours) +
@@ -28,7 +28,7 @@ ra.plot <- function(ra.df, gv, mycolours){
           panel.border = element_rect(fill = NA),
           plot.margin = unit(c(4,4,4,4), "pt")
       ) +
-      labs(y = "total abundance", x = "species rank") +
+      labs(y = "Total abundance", x = "Species rank") +
       scale_y_continuous(trans = scales::pseudo_log_trans(),
                        limits = c(0,(max(plot.df$sum_data))),
                        breaks = c(0, 10, 100, 1000)
